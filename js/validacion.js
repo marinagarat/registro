8744ac7a4a739b6978f2151b3de4cabef8925a40
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-08-11 15:04:06 -0300

    tarea en grupo

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,34 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+function validarForm(){   
+  let nombre = document.getElementById('nombre').value;
+  let apellido = document.getElementById('apellido').value;
+  let correo = document.getElementById('email').value;
+  let contraseña = document.getElementById('password1').value;
+  let contraseña2 = document.getElementById('password2').value;
+  let terminos = document.getElementById('terminos').checked; 
+
+  if (nombre.length > 0 || apellido.length > 0 || correo.length > 0 || contraseña.length > 0 || contraseña2.length > 0 || terminos) {
+    if (contraseña.length >= 6 && contraseña2.length >= 6){
+      if (contraseña === contraseña2) {
+        showAlertSuccess();
+      }  
+    } 
+  } else { 
+      showAlertError(); 
+    }
+}
+  
+  
+
+      document.addEventListener('DOMContentLoaded', function(){ 
+      document.getElementById('regBtn').addEventListener('click', validarForm()); 
+ 
+});
+
+          
+            
+  
